10a95ec0a25fd065dc81a54466bcc7a42544b385
khalid147369 <rabaaouikhalid@gmail.com>
2024-07-23 12:36:10 +0200

    edit

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ import express from "express";
 import mongoose from "mongoose";
 import "dotenv"
 import cors from "cors";
-import admindata from "./server/modules/admins.js";
+import admindata from "./modules/admins.js";
 import bcrypt from "bcrypt";
-import spacetoondb from "./server/routers/spacetoondata.js";
+import spacetoondb from "./routers/spacetoondata.js";
 import { body, validationResult } from "express-validator";
 import jwt from "jsonwebtoken";
-import usersdata from "./server/modules/users.js";
-import Favorites from "./server/routers/favorites.js";
-import usergoogle from "./server/modules/google.js";
+import usersdata from "./modules/users.js";
+import Favorites from "./routers/favorites.js";
+import usergoogle from "./modules/google.js";
 // ---------------google-imports
 import session from "express-session";
 import passport from "passport";
